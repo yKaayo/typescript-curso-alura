8c3ae4fb4823da3e59b6172f10bc7c357c2af152
yKaayo <caiokaea@gmail.com>
2025-03-18 20:47:07 -0300

    Aula05

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,16 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: Number;
-    private _valor: Number;
+  constructor(
+    private _data: Date,
+    private _quantidade: number,
+    private _valor: number
+  ) {}
 
-    constructor(data: Date, quantidade: Number, valor: Number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+  get data(): Date {
+    const data = new Date(this._data.getTime())
+    return data
+  }
 
-    get data(): Date {
-        return this._data;
-    }
-
-    get quantidade(): Number {
-        return this._quantidade;
-    }
-
-    get valor(): Number {
-        return this._valor;
-    }
-
-    get volume(): Number {
-        return this._quantidade * this._valor;
-    }
+  get volume(): number {
+    return this._quantidade * this._valor;
+  }
 }
